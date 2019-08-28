82c82ce03f983f7c586156faa860341216c14cec
tagadanar <raphael.renon@gmail.com>
2019-08-28 15:03:03 +0200

    Create colors.py

---
+++ gardener/colors.py
@@ -0,0 +1,9 @@
+class bcolors:
+	HEADER = '\033[95m'		# purple
+	OKBLUE = '\033[94m'		# blue
+	OKGREEN = '\033[92m'	# green
+	WARNING = '\033[93m' 	# yellow
+	FAIL = '\033[91m'		# red
+	ENDC = '\033[0m'		# end char
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
